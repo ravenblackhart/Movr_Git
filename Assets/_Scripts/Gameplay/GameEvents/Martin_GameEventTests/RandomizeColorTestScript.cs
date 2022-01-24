fd6b12228cae128c62b74b2fb75d8e803d911001
Raven Blackhart <raven@cafiend.com>
2022-01-24 13:24:34 +0100

    Post-Lunch Update

--- Assets/_Scripts/Gameplay/GameEvents/Martin_GameEventTests/RandomizeColorTestScript.cs
+++ Assets/_Scripts/Gameplay/GameEvents/Martin_GameEventTests/RandomizeColorTestScript.cs
@@ -1,13 +1,20 @@
+using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class RandomizeColorTestScript : MonoBehaviour
 {
     private Renderer _renderer;
-
+    private bool atTop = false;
+    private Vector3 _origin;
     private void Awake()
     {
         _renderer = GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        _origin = transform.position;
     }
 
     public void SetRandomColor()
@@ -18,5 +25,20 @@ public class RandomizeColorTestScript : MonoBehaviour
             Random.Range(0f, 1f));
 
         _renderer.material.color = color;
+    }
+
+    public void MovePosition()
+    {
+        SetRandomColor();
+        if (!atTop)
+        {
+            Vector3 offset = new Vector3(0, .5f, 0);
+            transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z) + offset;
+        }
+        else
+        {
+            transform.position = _origin;
+        }
+        atTop = !atTop;
     }
 }
