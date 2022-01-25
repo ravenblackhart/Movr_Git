b2e69497b2d2a1b694a2285c2e86fc037d6051ef
Raven Blackhart <raven@cafiend.com>
2022-01-25 12:11:56 +0100

    Pre-lunch Push

---
+++ Assets/_Scripts/Gameplay/TempAudioScripts/AudioManager2.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager2 : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    private AudioSource _clip;
+    private void Awake()
+    {
+        // if (instance == null)
+        //     instance = this;
+        // else
+        // {
+        //     Destroy(gameObject);
+        //     return;
+        // }
+        //
+        // DontDestroyOnLoad(gameObject);
+        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        _clip = GetComponent<AudioSource>();
+        _clip.Play();
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+        {
+            s.source.Play();
+        }
+        // // if (!s.source.isPlaying)
+        // // {
+        //     s.source.Play();
+        // // }
+    }
+}
--- Assets/_Scripts/JoelTestScrpts/PickUpTest.cs
+++ Assets/_Scripts/JoelTestScrpts/PickUpTest.cs
@@ -16,12 +16,32 @@ public class PickUpTest : MonoBehaviour
     Vector3 prevPosition;
     InputAction rightClick;
 
-    float xAxisRotation;
-    float yAxisRotation;
+    [SerializeField] float xAxisRotation;
+    [SerializeField] float yAxisRotation;
     CameraSwitcher cameraSwitcher;
-    float oldXRotation;
-    float oldYRotation;
+    [SerializeField] float oldXRotation;
+    [SerializeField] float oldYRotation;
     bool rotating;
+
+    private PlayerInput _playerInput;
+    [SerializeField] Vector2 mouseDelta;
+    [SerializeField] Camera mainCamera;
+
+
+    private void Awake() {
+        _playerInput = GetComponent<PlayerInput>();
+        rightClick = _playerInput.actions["SecondaryAction"];
+    }
+
+    private void OnEnable() {
+        rightClick.Enable();
+        rightClick.performed += OnSecondaryAction;
+    }
+
+    private void OnDisable() {
+        rightClick.Disable();
+        rightClick.performed -= OnSecondaryAction;
+    }
 
     void Start()
     {
@@ -40,39 +60,42 @@ public class PickUpTest : MonoBehaviour
 
     void Update()
     {
-        if (heldObject != null && Mouse.current.rightButton.isPressed) {
+        mouseDelta = _playerInput.actions["MouseLook"].ReadValue<Vector2>();
+        //Vector2 projectedMousePos = mainCamera.ScreenToWorldPoint(mousePosition);
+        
+
+        if (rotating) {
             RotateObject();
-            rotating = true;
+
+            //xAxisRotation = Mouse.current.position.x.ReadValue();
+            //yAxisRotation = Mouse.current.position.y.ReadValue();
+
+            xAxisRotation = mouseDelta.x;
+            yAxisRotation = mouseDelta.y;
         }
 
         else {
-            rotating = false;
-        }
 
-        if (!rotating) {
-            oldXRotation = Mouse.current.position.x.ReadValue();
-            oldYRotation = Mouse.current.position.y.ReadValue();
-            cameraSwitcher.UseMeTemporarily(false);
-        }
+            //oldXRotation = Mouse.current.position.x.ReadValue();
+            //oldYRotation = Mouse.current.position.y.ReadValue();
 
-        else {
-            xAxisRotation = Mouse.current.position.x.ReadValue();
-            yAxisRotation = Mouse.current.position.y.ReadValue();
-            cameraSwitcher.UseMeTemporarily(true);
-        }
-
-        //worldPos = mainCam.WorldToScreenPoint(Mouse.current.position);
+            oldXRotation = mouseDelta.x;
+            oldYRotation = mouseDelta.y;
+        }     
     }
 
     private void FixedUpdate() {
         if (heldObject != null) {
             MoveObject();           
         }
+
+        holdPos.transform.LookAt(transform.position);
     }
 
     public void Interact(GameObject obj) {
         if (heldObject == null) {
             PickupObject(obj);
+            print("picked up " + obj);
         }
 
         else {
@@ -86,7 +109,7 @@ public class PickUpTest : MonoBehaviour
         objRb.drag = drag;
         //objRb.constraints = RigidbodyConstraints.FreezeRotation;
 
-        /*objRb.transform.parent = holdPos;*/
+        objRb.transform.parent = holdPos;
         //objRb.transform.parent = physicsParent;
         heldObject = obj;
     }
@@ -95,11 +118,12 @@ public class PickUpTest : MonoBehaviour
         Rigidbody heldRb = heldObject.GetComponent<Rigidbody>();
         Rigidbody rb = transform.GetComponentInParent<Rigidbody>();
 
+
         if (Vector3.Distance(heldObject.transform.position, holdPos.position) > 0.01f) {
             Vector3 moveDirection = (holdPos.position - heldObject.transform.position);
             heldObject.GetComponent<Rigidbody>().AddForce(moveDirection * moveForce);
 
-            // Can we add car's velocity?
+            //Can we add car's velocity?
             
             //heldRb.velocity = rb.velocity + moveDirection * moveForce;
         }
@@ -109,6 +133,7 @@ public class PickUpTest : MonoBehaviour
         }
 
     }
+    
 
     public void ThrowObject() {
         Rigidbody heldRb = heldObject.GetComponent<Rigidbody>();
@@ -118,12 +143,39 @@ public class PickUpTest : MonoBehaviour
         heldRb.AddForce(holdPos.position - heldObject.transform.position * -pushForce);
         heldRb.constraints = RigidbodyConstraints.None;
 
-        /*heldRb.transform.parent = null;*/
+        heldRb.transform.parent = null;
         heldObject = null;
     }
 
-    void RotateObject() {        
-        heldObject.transform.Rotate(Vector3.right, yAxisRotation * rotateSpeed - oldYRotation * rotateSpeed, Space.World);
-        heldObject.transform.Rotate(Vector3.down, xAxisRotation * rotateSpeed - oldXRotation * rotateSpeed, Space.World);
+    void RotateObject() {
+
+        //heldObject.transform.Rotate(Vector3.right, yAxisRotation * rotateSpeed - oldYRotation * rotateSpeed, Space.World);
+        //heldObject.transform.Rotate(Vector3.right, mouseDelta.y * rotateSpeed, Space.World);
+        //heldObject.transform.Rotate(Vector3.down, mouseDelta.x * rotateSpeed, Space.World);
+        //heldObject.transform.Rotate(Vector3.down, xAxisRotation * rotateSpeed - oldXRotation * rotateSpeed, Space.World);
+
+        heldObject.transform.RotateAround(holdPos.position, Vector3.down, mouseDelta.x * rotateSpeed);
+        heldObject.transform.RotateAround(holdPos.position, -holdPos.transform.right, mouseDelta.y * rotateSpeed);
+
     }
+
+    private void OnSecondaryAction(InputAction.CallbackContext context) {
+        if (heldObject != null) {
+            StartCoroutine(LockCamera());
+        }
+    }
+
+    IEnumerator LockCamera() {
+
+        cameraSwitcher.ToggleLock();
+        rotating = true;
+
+        while (rightClick.ReadValue<float>() != 0) {
+            yield return null;
+        }
+
+        cameraSwitcher.ToggleLock();
+        rotating = false;
+    }
+
 }
