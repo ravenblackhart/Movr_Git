e22bec618f493b9fe7632c160630f0a50e1c767d
Raven Blackhart <raven@cafiend.com>
2022-01-25 09:12:39 +0100

    start of day

---
+++ Assets/_Scripts/UI/CrossHair.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossHair : MonoBehaviour
+{
+    #region Declarations
+
+    [Header("Crosshair Sprites")] 
+    [SerializeField] private Image defaultCrosshair;
+    [SerializeField] private Image pickupsCrosshair;
+    [SerializeField] private Image interactCrosshair;
+    #endregion
+    
+    
+}
