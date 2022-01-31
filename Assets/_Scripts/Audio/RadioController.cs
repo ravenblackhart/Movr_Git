2a2890dc72f92108d827c41562d0c775efff9335
Raven Blackhart <raven@cafiend.com>
2022-01-31 08:56:28 +0100

    Day Start

---
+++ Assets/_Scripts/Audio/RadioController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioController : MonoBehaviour
+{
+    private AudioSource radioAudioSource;
+
+    private void Start()
+    {
+        radioAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio()
+    {
+        radioAudioSource.Play();
+    }
+
+    public void PauseAudio()
+    {
+        radioAudioSource.Pause();
+    }
+
+    public void StopAudio()
+    {
+        radioAudioSource.Stop();
+    }
+
+}
