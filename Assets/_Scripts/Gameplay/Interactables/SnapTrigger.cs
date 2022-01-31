30b17a8f6a66b0acbbdd4a6e211262221d5aeab7
Raven Blackhart <raven@cafiend.com>
2022-01-31 16:40:57 +0100

    Perforce Sync

---
+++ Assets/_Scripts/Gameplay/Interactables/SnapTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SnapTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform _snapPosition;
+    public Transform SnapPosition => _snapPosition;
+    
+    
+    // private void OnTriggerStay(Collider other) {
+    //     if (other.CompareTag("CassetteTrigger")) {
+    //         _onTrigger = true;
+    //         trigger = other.transform;
+    //         _playerPickUp.hoverObj = trigger;
+    //         _playerPickUp.hoverDistance = true;
+    //     }
+    // }
+    //
+    // private void OnTriggerExit(Collider other) {
+    //     if (other.CompareTag("CassetteTrigger")) {
+    //         _onTrigger = false;
+    //         trigger = null;
+    //         _playerPickUp.hoverDistance = false;
+    //     }
+    // }
+}
---
+++ Assets/_Scripts/Task Scripts/ChangeMusicTask.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusicTask : Task
+{
+    public override void StartTask(GameManager gameManager)
+    {
+        //
+    }
+
+    public override void UpdateTask(GameManager gameManager)
+    {
+        //
+    }
+
+    public override void EndTask(GameManager gameManager)
+    {
+        //
+    }
+
+    public override bool CheckValid(GameManager gameManager)
+    {
+        return true;
+    }
+}
