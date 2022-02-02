ad74cb8a23317413ff14e2e5a9198379ab85035b
Raven Blackhart <raven@cafiend.com>
2022-02-02 23:35:40 +0100

    Audio Mixer + Audio Manager

--- Assets/_Scripts/Managers/AudioManager.cs
+++ Assets/_Scripts/Managers/AudioManager.cs
@@ -1,10 +1,14 @@
 using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioMixer Mixer;
+    public AudioSetting[] AudioSettings; 
+    private enum AudioGroups {Global, Music, SFX, Environment }
     public Sound[] sounds;
 
     public static AudioManager instance;
@@ -30,7 +34,15 @@ public class AudioManager : MonoBehaviour
             s.source.loop = s.loop;
         }
     }
-    
+
+    private void Start()
+    {
+        for (int i = 0; i < AudioSettings.Length; i++)
+        {
+            AudioSettings[i].Initialize();
+        }
+    }
+
     public void Play(string name) {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s != null) {
@@ -40,5 +52,80 @@ public class AudioManager : MonoBehaviour
         // // {
         //     s.source.Play();
         // // }
+    }
+
+    public void GlobalSlider(float value)
+    {
+        AudioSettings[(int)AudioGroups.Global].SetExposedParam(value); 
+    }
+    
+    public void MusicSlider(float value)
+    {
+        AudioSettings[(int)AudioGroups.Music].SetExposedParam(value); 
+    }
+    
+    public void SFXSlider(float value)
+    {
+        AudioSettings[(int)AudioGroups.SFX].SetExposedParam(value); 
+    }
+    public void EnvSlider(float value)
+    {
+        AudioSettings[(int)AudioGroups.Environment].SetExposedParam(value); 
+    }
+
+    public void GlobalToggle()
+    {
+        AudioSettings[(int)AudioGroups.Global].ToggleAudio();
+    }
+    
+    public void MusicToggle()
+    {
+        AudioSettings[(int)AudioGroups.Music].ToggleAudio();
+    }
+    
+    public void SFXToggle()
+    {
+        AudioSettings[(int)AudioGroups.SFX].ToggleAudio();
+    }
+    
+    public void EnvToggle()
+    {
+        AudioSettings[(int)AudioGroups.Environment].ToggleAudio();
+    }
+    
+    
+}
+
+[System.Serializable]
+public class AudioSetting
+{
+    public Slider slider;
+    public GameObject redX;
+    public string exposedParam;
+    public Toggle soundToggle; 
+
+    public void Initialize()
+    {
+        slider.value = PlayerPrefs.GetFloat(exposedParam);
+    }
+
+    public void ToggleAudio()
+    {
+        if (!soundToggle)
+        {
+            slider.value = PlayerPrefs.GetFloat(exposedParam);
+        }
+        
+        if (soundToggle)
+        {
+            slider.value = slider.minValue; 
+        }
+    }
+    
+    public void SetExposedParam(float value)
+    {
+        redX.SetActive(value <= slider.minValue);
+        AudioManager.instance.Mixer.SetFloat(exposedParam, value);
+        PlayerPrefs.SetFloat(exposedParam, value);
     }
 }
