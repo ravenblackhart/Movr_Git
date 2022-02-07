c3af6a0e2599f26d6a4fd5034522b19fcaa2c80d
Raven Blackhart <raven@cafiend.com>
2022-02-07 19:18:18 +0100

    Sync

--- Assets/_Scripts/Gameplay/Interactables/LeverGearstick.cs
+++ Assets/_Scripts/Gameplay/Interactables/LeverGearstick.cs
@@ -13,19 +13,16 @@ public class LeverGearstick : Lever
     void Awake()
     {
         _playerDrag = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDragObject>();
-        //_dragSpeed = 2f;
-        //Debug.LogWarning("funkar>!!" + _playerDrag);
     }
 
     public override void UpdateLeverValue()
     {
-        // How much does the lever value change, script GearStick reads it (LeverValue)
+        // How much does the lever value change depending on mouse movement,
+        // script GearStick reads it (the LeverValue)
         _leverValue -= _moveDirection * Time.deltaTime;
-        
         _leverValue = Mathf.Clamp(_leverValue, -0.5f, 1);
     }
-    // clamp -60, -40,  -20,  0,   15    30    
-    // value   1, 0.66, 0.33, 0, -0.25, -0.5
+
     public override void UpdateLeverTransform()
     {
         // Change the lever value to degrees
@@ -36,7 +33,11 @@ public class LeverGearstick : Lever
             _orgin.transform.localEulerAngles.y,  SetStickAngle( -_zAngle));//-_zAngle);//
         
     }
-
+    /// <summary>
+    /// Gives the stick four gears: full speed, half speed, park(brake) and reverse
+    /// </summary>
+    /// <param name="curValue"></param>
+    /// <returns></returns>
     private float SetStickAngle(float curValue)
     {
         if (curValue < -55)
@@ -44,12 +45,12 @@ public class LeverGearstick : Lever
             _leverValue = 1;
             return -60;
         }
-        else if (curValue < -25 && curValue > -35)
+        else if (curValue < -25 && curValue > -40)
         {
             _leverValue = 0.5f;
             return -30;
         }
-        else if (curValue < 5 && curValue > -5)
+        else if (curValue < 10 && curValue > -5)
         {
             _leverValue = 0;
             return 0;
@@ -63,38 +64,6 @@ public class LeverGearstick : Lever
         {
             return curValue;
         }
-        //int arrayIndex = 0;
-        //float prevGear = float.MinValue;
-        //foreach (float a in _gearAngles)
-        //{
-        //    if (curValue <= a && curValue > prevGear)
-        //    {
-        //        //StartCoroutine(ResetStick());
-        //        //_leverValue = SetLeverValue(arrayIndex);
-        //        //return a;
-        //        Debug.LogWarning(a + " " + prevGear + " " + curValue + " " + _moveDirection);
-        //        float diffA = a - curValue;
-        //        float diffB = curValue - prevGear;
-        //        if (_moveDirection < 0)
-        //        {
-        //            //Debug.LogWarning(a + " a " + curValue);
-        //            return a;
-        //        }
-        //        else if (_moveDirection > 0)
-        //        {
-        //            //Debug.LogWarning(prevGear + " b " + curValue);
-        //            return prevGear;
-        //        }
-        //    }
-        //    arrayIndex++;
-        //    prevGear = a;
-        //}
-        //    Debug.LogWarning(curValue);
-        //if (curValue < 0)
-        //{
-        //    return -60;
-        //}
-        //return 30;
     }
 
 }
